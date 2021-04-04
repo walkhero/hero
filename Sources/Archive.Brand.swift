231947941f1d81a3446bcfbb89822954b1e72198
vaux <zero.griffin@gmail.com>
2021-04-04 13:02:34 +0200

    Reverse brand

--- Sources/Archive.Brand.swift
+++ Sources/Archive.Brand.swift
@@ -4,16 +4,17 @@ extension Archive {
     public static var brand: Self {
         var archive = Self()
         archive.challenges = .init(Challenge.allCases)
-        archive.walks = (0 ..< 70)
+        archive.walks = (1 ..< 70)
             .filter { _ in
                 Int.random(in: 0 ..< 2) == 0
             }
             .map {
                 .init(date: Calendar.current.date(byAdding: .day, value: -$0, to: .init())!,
-                     duration: .random(in: 15 * 60 ..< 180 * 60),
-                     steps: .random(in: 200 ..< 9000),
-                     metres: .random(in: 200 ..< 6000))
+                     duration: .random(in: 50 * 60 ..< 180 * 60),
+                     steps: .random(in: 2000 ..< 9000),
+                     metres: .random(in: 2000 ..< 6000))
             }
+            .reversed()
         archive.date = .init()
         return archive
     }
