e0ec1b16b762b9ab5947cb198606c0ab3f728867
vaux <zero.griffin@gmail.com>
2021-04-12 11:43:14 +0200

    Make tile public

--- Sources/Constants.swift
+++ Sources/Constants.swift
@@ -3,7 +3,7 @@ import MapKit
 public struct Constants {
     public struct map {
         public static let zoom = 20
-        static let tile = with(zoom: zoom)
+        public static let tile = with(zoom: zoom)
         
         static func with(zoom: Int) -> Double {
             MKMapRect.world.width / pow(2, .init(zoom))
