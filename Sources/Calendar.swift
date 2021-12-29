6bd830755bd9a54860ae2025433c4c52bff235f2
vaux <zero.griffin@gmail.com>
2021-12-29 15:23:06 +0100

    Update week

--- Sources/Calendar.swift
+++ Sources/Calendar.swift
@@ -5,6 +5,18 @@ extension Calendar {
         component(.day, from: date(byAdding: .day, value: -1, to: dateInterval(of: .month, for: .init())!.end)!) - component(.day, from: .init())
     }
     
+    public func leadingWeekdays(year: Int, month: Int, day: Int) -> Int {
+        {
+            $0 < firstWeekday ? 7 - firstWeekday + $0 : $0 - firstWeekday
+        } (component(.weekday, from: date(from: .init(year: year, month: month, day: day))!))
+    }
+    
+    public func trailingWeekdays(year: Int, month: Int, day: Int) -> Int {
+        {
+            $0 < firstWeekday ? firstWeekday - 1 - $0 : 6 + firstWeekday - $0
+        } (component(.weekday, from: date(from: .init(year: year, month: month, day: day))!))
+    }
+    
     func duration(from timestamp: UInt32) -> UInt16 {
         var finish = Date.now
         let start = Date(timestamp: timestamp)
