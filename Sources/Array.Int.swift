0377e5c20b12c8d4e9c4a1daf9b49a0b4d0749f8
vaux <zero.griffin@gmail.com>
2022-01-12 00:31:16 +0100

    Update chart

--- Sources/Array.Int.swift
+++ Sources/Array.Int.swift
@@ -4,9 +4,9 @@ extension Array where Element == Int {
     var chart: Chart {
         { max, sum in
             .init(values: map { max > 0 ? .init($0) / max : 0 },
-                  total: .init(sum),
+                  total: sum,
                   max: Int(max),
-                  average: isEmpty ? 0 : .init(ceil(sum / .init(count))))
-        } (Double(self.max() ?? 0), .init(reduce(0, +)))
+                  average: isEmpty ? 0 : .init(ceil(.init(sum) / Double(count))))
+        } (Double(self.max() ?? 0), reduce(0, +))
     }
 }
