ee97c6609662d5e22358ec8512757b3a9b929f2f
vaux <zero.griffin@gmail.com>
2022-06-13 15:19:11 +0200

    Update defaults

--- Sources/Defaults.swift
+++ Sources/Defaults.swift
@@ -2,34 +2,33 @@ import Foundation
 
 public enum Defaults: String {
     case
-    rated,
     created,
     premium,
     hide
 
-    public static var action: Action {
+    public static var froob: Bool {
         if let created = wasCreated {
-            let days = Calendar.global.dateComponents([.day], from: created, to: .init()).day!
-            if !hasRated && days > 4 {
-                hasRated = true
-                return .rate
-            } else if hasRated && !isPremium && days > 5 {
-                return .froob
-            }
+            let days = Calendar.current.dateComponents([.day], from: created, to: .init()).day!
+            return !isPremium && days > 2
         } else {
             wasCreated = .init()
         }
-        return .none
+        return false
+    }
+    
+    public static var rate: Bool {
+        if let created = wasCreated {
+            let days = Calendar.current.dateComponents([.day], from: created, to: .init()).day!
+            return days > 1
+        } else {
+            wasCreated = .init()
+        }
+        return false
     }
     
     public static var isPremium: Bool {
         get { self[.premium] as? Bool ?? false }
         set { self[.premium] = newValue }
-    }
-    
-    public static var hasRated: Bool {
-        get { self[.rated] as? Bool ?? false }
-        set { self[.rated] = newValue }
     }
     
     public static var shouldHide: Bool {
